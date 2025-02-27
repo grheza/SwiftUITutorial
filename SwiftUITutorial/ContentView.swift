64d37fc16a1f6ab7121020f4a120d853edb09b5b
RhezaWidhiarto <rheza.widhiarto@allianz.co.id>
2025-02-27 23:13:18 +0700

    Init project

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -9,16 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("bg-login")
+                .resizable()
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                IconView()
+                FormLogin()
+            }
+            .padding(.all, 20)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+// TODO: Can select weather use this hashtag or struct (above)
+//#Preview {
+//    ContentView()
+//}
+
+struct IconView: View {
+    var body: some View {
+        VStack {
+            Image("swift-icon")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .background(.profileBackground)
+                .foregroundColor(.white)
+                .padding()
+                .background(.profileBackground)
+                .cornerRadius(20)
+            Text("Please Login")
+                .bold()
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct FormLogin: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Username")
+                .font(.callout)
+                .bold()
+            TextField("Input your username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Text("Password")
+                .font(.callout)
+                .bold()
+            SecureField("Input your password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: { print("Hello \($username.wrappedValue)") }) {
+                Text("SIGN IN")
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding()
+            .background(.black)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            
+        }
+        .padding(.all, 30)
+        .background(.profileBackground)
+        .cornerRadius(10)
+        
+    }
 }
