02a9bfc8dd875610ae34818f534efc54039a58d6
RhezaWidhiarto <rheza.widhiarto@allianz.co.id>
2025-02-27 15:13:56 +0700

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by RhezaWidhiarto on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
